0fa15bf525d4d1ceb2c1024e5a98051192f4d2b6
Darrian Ward <aerialdino@gmail.com>
2025-03-13 22:33:40 +0000

    Starting skeleton of controller

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.*;
+import com.example.entity.*;
+import com.example.service.*;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +10,18 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
 
+    SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/register")
+    public void createAccount(@RequestBody Account account) {
+        
+    }
 }
